e37c1dfc20f9b639e2c536ba4e25f0e47737a28d
Harnnarong <harnnarong@gmail.com>
2020-04-29 10:26:20 +0700

    remote all unused plugins

--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
-import { WindowScrollController } from '@fullcalendar/core';
+
 
 declare const App : any;
 
